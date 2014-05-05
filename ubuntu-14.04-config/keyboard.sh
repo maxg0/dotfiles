87b1674aa25bf5ff2653da3ccf3713acb994ac11
Max Gräsbeck <grasbecm@arcada.fi>
2014-05-05 15:11:30 +0300

    disable super+w super+shift+w

--- ubuntu-14.04-config/keyboard.sh
+++ ubuntu-14.04-config/keyboard.sh
@@ -8,4 +8,9 @@ gconftool-2 --set "/apps/compiz-1/plugins/unityshell/screen0/options/show_launch
 # http://customizeubuntu.com/preferences/keyboard-shortcuts/disable-hud-when-pressing-alt-key
 gconftool-2 --set "/apps/compiz-1/plugins/unityshell/screen0/options/show_hud" --type string ""
 
+# disable super+w
+gconftool-2 --set "/apps/compiz-1/plugins/scale/screen0/options/initiate_key" --type string ""
+# super+shift+w
+gconftool-2 --set "/apps/compiz-1/plugins/scale/screen0/options/initiate_all_key" --type string ""
+
 gsettings set org.gnome.desktop.wm.keybindings panel-main-menu []
