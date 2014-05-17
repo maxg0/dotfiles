6c7b739d5e79d25dbf93c3cc3dd405e3231ac908
Max Grsäsbeck <grasbecm@arcada.fi>
2014-05-17 20:28:21 +0300

    fix for apt-add-repository: it needs a string as argument; removed "non-free" from the repo string and now it seems to work

--- chrome.sh
+++ chrome.sh
@@ -3,7 +3,7 @@
 # Installs Google Chrome
 # http://askubuntu.com/a/79284/194406
 
-apt-add-repository deb http://dl.google.com/linux/deb/ stable non-free main
+sudo apt-add-repository 'deb http://dl.google.com/linux/deb/ stable main'
 
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
