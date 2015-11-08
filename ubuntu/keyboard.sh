f62a98305d40c2883cfcdff46622b42bce17e770
Max <maxgrasbeck@gmail.com>
2015-11-08 17:32:50 +0200

    Add function keys map
    A map of what the different function keys do is needed to avoid
    assigning the same key twice (if one is forgotten for instance)

--- ubuntu/keyboard.sh
+++ ubuntu/keyboard.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+## Map of what the function keys are supposed to do
+# F1  is ingrained as the help key in every piece of software on the planet
+#     so I won't bother trying to change it
+# F2  is unassigned
+# F3  is unassigned
+# F4  switches between cpp and h files (vimrc)
+# F5  is unassigned
+# F6  is unassigned
+# F7  is unassigned
+# F8  is unassigned
+# F9  is media pause # TODO automatically set this
+# F10 is media next  # TODO automatically set this
+# F11 is for setting fullscreen mode
+# F12 is for toggling paste mode in vim (vimrc)
+
+# TODO Shift+Alt L switch to next source
+# I tend to switch between a Finnish keyboard layout with umlauts
+# and an American layout for programming
+
 # Turn capslock into escape
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
