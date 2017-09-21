91d63c59f0754017329a75db6866fa5b557bc649
Max <maxgrasbeck@gmail.com>
2017-09-21 18:41:42 +0300

    Monitor toggle variables

--- monitorToggle.sh
+++ monitorToggle.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# this script is very system dependent and will probably not work
+# You need to set these to the corresponding monitors of your setup.
+# You can figure them out with `xrandr` and guessing.
+primary=DVI-I-2
+secondary=DVI-I-3
 
-monitorStatus=$(xrandr | grep DVI-I-1 | sed "s/.* connected //g" | sed "s/(.*)//g")
+monitorStatus=$(xrandr | grep $secondary | sed "s/.* connected //g" | sed "s/(.*)//g")
 
 if [ "$monitorStatus" != "" ]; then
     # monitor is on, so we turn it off
-    xrandr --output DVI-I-2 --auto --output DVI-I-1 --off
+    xrandr --output $primary --auto --output $secondary --off
 else
     # monitor is off, so we turn it on
-    xrandr --output DVI-I-2 --auto --output DVI-I-1 --auto --right-of DVI-I-2
+    xrandr --output $primary --auto --output $secondary --auto --right-of $primary
 fi
